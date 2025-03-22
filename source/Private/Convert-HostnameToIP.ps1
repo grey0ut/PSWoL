e7cca2bf027db4b98f5a6bf2ddfd96d5b0c7ab5d
Courtney Bodett <bode235@lni.wa.gov>
2025-03-22 16:53:01 -0700

    updated help changed output to null on fail

--- source/Private/Convert-HostnameToIP.ps1
+++ source/Private/Convert-HostnameToIP.ps1
@@ -4,8 +4,14 @@ function Convert-HostnameToIP {
     Convert a provided hostname to its resolved IP address
     .DESCRIPTION
     We need an IPv4 address in order to get a MAC for WOL. This function attempts to convert a hostname in to an IP address
+    .PARAMETER ComputerName
+    A computer name to target for WOL.  Will attempt to resolve the name to an IP address that will then be queried in ARP to determine a MAC.
+    .EXAMPLE
+    Convert-HostnameToIP 'ContosoComp'
+    192.168.15.10
     #>
     [CmdletBinding()]
+    [OutputType([String])]
     param (
         [String]$ComputerName
     )
@@ -15,6 +21,6 @@ function Convert-HostnameToIP {
             $_ -notmatch ':'
         }
     } catch {
-        return "NotFound"
+        return $null
     }
 }
