f663208e3778f30b89ce5e0631512140e5e05d23
Courtney Bodett <bode235@lni.wa.gov>
2025-03-22 16:52:47 -0700

    added output type

--- source/Private/Resolve-TargetType.ps1
+++ source/Private/Resolve-TargetType.ps1
@@ -4,8 +4,14 @@ function Resolve-TargetType {
     Determine if a provided string is an IP address, MAC address, or computername (possibly)
     .DESCRIPTION
     Takes an input string and returns a string value representing whether the input was an IP address, MAC address, or computername
+    .PARAMETER Target
+    Takes the provided target for a WOL packet and determines if it's a MAC address, IP address or computer name.
+    .EXAMPLE
+    Resolve-TargetType 192.168.15.10
+    IPAddress
     #>
     [CmdletBinding()]
+    [OutputType([String])]
     param (
         [String]$Target
     )
