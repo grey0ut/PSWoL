4182c91fbd72cd779ebc4493f49471a7f9eeb941
cbodett <courtney.bodett@gmail.com>
2025-03-23 20:30:21 -0700

    added ability to call saved targets

--- source/Public/Send-WakeOnLan.ps1
+++ source/Public/Send-WakeOnLan.ps1
@@ -7,8 +7,10 @@ function Send-WakeOnLan {
     .PARAMETER TargetAddress
     Target address to send Wake-On-LAN packet to. Accepts IP address, MAC address or Computer Name.
     IP must be an IPv4 address.
-    MAC address can be '1234.abde.4321', '12:34:AB:DE:43:21', '12-34-AB-DE-32-21' or '1234ABDE4321' format.
+    MAC addresses can be '1234.abde.4321', '12:34:AB:DE:43:21', '12-34-AB-DE-32-21' or '1234ABDE4321' format.
     Computername needs to be able to resolve to a local computer name. If resolution fails, the packet will not send.
+    .PARAMETER SavedTarget
+    If there are currently saved target addresses via Save-WolTarget you can specify their name(s) with -SavedTarget to send a WoL packet to them.
     .EXAMPLE
     PS> Send-WakeOnLan -TargetAddress '192.168.15.10'
 
@@ -17,11 +19,17 @@ function Send-WakeOnLan {
     PS> 'E2-5D-0E-B5-E2-E8', 'DD-20-C9-FF-EC-79', '6D-84-01-BC-D2-CD' | Send-WakeOnLan
 
     # this will send a WOL packet to each of the 3 MAC addresses.
+    .EXAMPLE
+    PS> Send-WakeOnLan -SavedTarget "TheGibson"
+
+    # this will check the saved targets for the name "TheGibson" and send a WoL packet to the MAC address associated with that name.
     #>
     [CmdletBinding(SupportsShouldProcess)]
     param (
-        [Parameter(ValueFromPipeline,Mandatory)]
-        [String[]]$TargetAddress
+        [Parameter(ValueFromPipeline,Mandatory,ParameterSetName="TargetAddress")]
+        [String[]]$TargetAddress,
+        [Parameter(ValueFromPipeline,Mandatory,ParameterSetName="SavedTarget")]
+        [String[]]$SavedTarget
     )
 
     begin {
@@ -31,56 +39,76 @@ function Send-WakeOnLan {
     }
 
     process {
-        foreach ($Target in $TargetAddress) {
-            switch (Resolve-TargetType -Target $Target) {
-                'IPAddress' {
-                    $ResolvedMac = Resolve-IPToMac -IPAddress $Target
-                    if ($ResolvedMac) {
-                        $MacBytes = Convert-MacToByte -MacAddress $ResolvedMac
-                        [Void]$WolTargets.Add(
-                            [PSCustomObject]@{
-                                Identifier = $Target
-                                MacAddress = $([System.BitConverter]::ToString($MacBytes))
-                                Packet = [Byte[]]$($BasePacket; $MacBytes * 16)
+        switch ($PSCmdlet.ParameterSetName) {
+            'Targetaddress' {
+                foreach ($Target in $TargetAddress) {
+                    switch (Resolve-TargetType -Target $Target) {
+                        'IPAddress' {
+                            $ResolvedMac = Resolve-IPToMac -IPAddress $Target
+                            if ($ResolvedMac) {
+                                $MacBytes = Convert-MacToByte -MacAddress $ResolvedMac
+                                [Void]$WolTargets.Add(
+                                    [PSCustomObject]@{
+                                        Identifier = $Target
+                                        MacAddress = $([System.BitConverter]::ToString($MacBytes))
+                                        Packet = [Byte[]]$($BasePacket; $MacBytes * 16)
+                                    }
+                                )
+                            } else {
+                                Write-Warning "Unable to determine MAC for $Target"
                             }
-                        )
-                    } else {
-                        Write-Warning "Unable to determine MAC for $Target"
-                    }
-                }
-                'MacAddress' {
-                    $MacBytes = Convert-MacToByte -MacAddress $Target
-                    if ($MacBytes) {
-                        [Void]$WolTargets.Add(
-                            [PSCustomObject]@{
-                                Identifier = $Target
-                                MacAddress = $([System.BitConverter]::ToString($MacBytes))
-                                Packet = [Byte[]]$($BasePacket; $MacBytes * 16)
-                            }
-                        )
-                    } else {
-                        Write-Warning "Unable to convert MAC to bytes: $Target"
-                    }
-                }
-                'ComputerName' {
-                    $ResolvedIP = Convert-HostnameToIP -ComputerName $Target
-                    if ($ResolvedIP) {
-                        $Mac = Resolve-IPToMac -IPAddress $ResolvedIP
-                        if ($Mac) {
-                            $MacBytes = Convert-MacToByte -MacAddress $Mac
-                            [Void]$WolTargets.Add(
-                                [PSCustomObject]@{
-                                    Identifier = $Target
-                                    MacAddress = $([System.BitConverter]::ToString($MacBytes))
-                                    Packet = [Byte[]]$($BasePacket; $MacBytes * 16)
-                                }
-                            )
-                        } else {
-                            Write-Warning "Unable to determine MAC for $ResolvedIP"
                         }
-                    } else {
-                        Write-Warning "Unable to resolve IP for $Target"
+                        'MacAddress' {
+                            $MacBytes = Convert-MacToByte -MacAddress $Target
+                            if ($MacBytes) {
+                                [Void]$WolTargets.Add(
+                                    [PSCustomObject]@{
+                                        Identifier = $Target
+                                        MacAddress = $([System.BitConverter]::ToString($MacBytes))
+                                        Packet = [Byte[]]$($BasePacket; $MacBytes * 16)
+                                    }
+                                )
+                            } else {
+                                Write-Warning "Unable to convert MAC to bytes: $Target"
+                            }
+                        }
+                        'ComputerName' {
+                            $ResolvedIP = Convert-HostnameToIP -ComputerName $Target
+                            if ($ResolvedIP) {
+                                $Mac = Resolve-IPToMac -IPAddress $ResolvedIP
+                                if ($Mac) {
+                                    $MacBytes = Convert-MacToByte -MacAddress $Mac
+                                    [Void]$WolTargets.Add(
+                                        [PSCustomObject]@{
+                                            Identifier = $Target
+                                            MacAddress = $([System.BitConverter]::ToString($MacBytes))
+                                            Packet = [Byte[]]$($BasePacket; $MacBytes * 16)
+                                        }
+                                    )
+                                } else {
+                                    Write-Warning "Unable to determine MAC for $ResolvedIP"
+                                }
+                            } else {
+                                Write-Warning "Unable to resolve IP for $Target"
+                            }
+                        }
                     }
+                }
+            }
+            'SavedTarget' {
+                $SavedTargets = foreach ($Target in $SavedTarget) {
+                    Get-WolTarget -Name $Target
+                }
+
+                foreach ($TargetObj in $SavedTargets) {
+                    $MacBytes = Convert-MacToByte -MacAddress $($TargetObj.MAC)
+                    [Void]$WolTargets.Add(
+                        [PSCustomObject]@{
+                            Identifier = $TargetObj.Name
+                            MacAddress = $([System.BitConverter]::ToString($MacBytes))
+                            Packet = [Byte[]]$($BasePacket; $MacBytes * 16)
+                        }
+                    )
                 }
             }
         }
